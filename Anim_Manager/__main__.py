9ac13a8677a07e34bad89685130c1e767dbdcd4e
Count Dracula <87222323+CDraculaX@users.noreply.github.com>
2021-10-07 10:15:55 +0530

    Create __main__.py

--- Anim_Manager/__main__.py
+++ Anim_Manager/__main__.py
@@ -154,11 +154,11 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="❓ Help ❓",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="My 𝙽𝚎𝚠𝚜 🙋",
-                                                                       url="https://t.me/senuinfinity")],
+                                                                       url="https://t.me/NET_HACKER_BOTs")],
                                                                                    [InlineKeyboardButton(text="➕ Add To Group ➕",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="𝚂𝚞𝚙𝚙𝚘𝚛𝚝 💬",
-                                                                       url="https://t.me/senuinfinitygroup")
+                                                                       url="https://t.me/NET_HACKER_BOTs_chat")
                                                                                  ]]))
 
     else:
